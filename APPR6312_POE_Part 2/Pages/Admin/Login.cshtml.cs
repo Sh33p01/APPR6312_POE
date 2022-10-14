c9a248c5a1b0183a7d11b93294efbe17775205db
Sh33p01 <Kenan.m001@gmail.com>
2022-10-14 20:22:40 +0200

    Added Back buttons to all pages

--- APPR6312_POE_Part 2/Pages/Admin/Login.cshtml.cs
+++ APPR6312_POE_Part 2/Pages/Admin/Login.cshtml.cs
@@ -18,8 +18,50 @@ namespace APPR6312_POE_Part_1.Pages.Admin
 
         }
 
-        
+        public void OnPost()
+        {
+            string email = Request.Form["uEmail"];
+            string pass = Request.Form["uPass"];
 
-        
+
+
+            string connectionString = "Server=tcp:appr6312-poe-part1.database.windows.net,1433;Initial Catalog=APPR6312-POE;Persist Security Info=False;User ID=ST10118069;Password=AdminPass1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM ALogin";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            string em = Convert.ToString(reader["Email"]);
+                            string ps = Convert.ToString(reader["Pass"]);
+
+
+
+                            if (email == em && pass == ps)
+                            {
+                                Response.Redirect("/Admin/AdminChoice");
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
     }
 }
